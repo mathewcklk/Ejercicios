afbb90eaf27d19defce5e7da597e6eba45d569a5
mathewcklk <mathew.carrillo@intsuperior.edu.ec>
2023-05-01 19:12:02 -0500

    Calcula la suma de los números enteros del 1 al 100 y mostrar el resultado.

---
+++ src/ec/edu/intsuperior/vista/Ejercicio4.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.intsuperior.vista;
+
+/**
+ *
+ * @author Mathew
+ */
+public class Ejercicio4 {
+    public static void main(String[] args) {
+        int suma = 0;
+for (int i = 1; i <= 100; i++) {
+    suma += i;
+}
+System.out.println("La suma de los números del 1 al 100 es: " + suma);
+
+    }
+}
